c009fa6fa589333dc1e33de312b10722ecd83a59
phscosta <phscosta@protonmail.com>
2021-11-12 14:36:29 -0300

    Adicionando a Solução e a Main

---
+++ ucl_elevador_ssup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ucl_elevador_ssup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
